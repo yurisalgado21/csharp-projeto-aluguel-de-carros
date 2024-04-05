84be82d63cb86e8a753cd05628e57b498c3d8146
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2024-04-05 14:56:01 -0300

    req 3 done

--- src/RentCars/Models/Vehicle.cs
+++ src/RentCars/Models/Vehicle.cs
@@ -1,7 +1,24 @@
+using System.Dynamic;
 using RentCars.Types;
 namespace RentCars.Models;
 
 //3 - Crie a classe `Vehicle`
 public class Vehicle
 {
+    public string Brand { get; set;}
+    public string Model { get; set; }
+    public decimal Price { get; set; }
+    public FuelType Fuel { get; set; }
+    public int EngineCapacity { get; set; }
+    public Color MainColor { get; set; }
+    public int Year { get; set; }
+    public double PricePerDay { get; set; }
+    public bool IsRented { get; set; }
+
+    public Vehicle()
+    {
+        Brand = "";
+        Model = "";
+        IsRented = false;
+    }
 }
